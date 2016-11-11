629a2b53ff67b765c62b3e29eebe1a5a65a10d93
Calvin Leung Huang <cleung2010@gmail.com>
2016-11-11 16:03:57 -0500

    Working unirest example

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:7
 COPY target /usr/src/myapp
 WORKDIR /usr/src/myapp
 # RUN javac HelloWorld.java
-CMD ["java", "-cp", "sample-app-1.0-SNAPSHOT.jar", "com.cimpress.sampleapp.App"]
+CMD ["java", "-jar", "sample-app-1.0-SNAPSHOT.jar"]
--- src/main/java/com/cimpress/sampleapp/App.java
+++ src/main/java/com/cimpress/sampleapp/App.java
@@ -1,13 +1,26 @@
 package com.cimpress.sampleapp;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+
 /**
- * Hello world!
+ * Sample application
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main( String[] args ) throws UnirestException {
+    HttpResponse<JsonNode> stack= Unirest.get("https://api.stackexchange.com/2.2/questions").
+      header("accept",  "application/json").
+      queryString("order","desc").
+      queryString("sort", "creation").
+      queryString("filter", "default").
+      queryString("site", "stackoverflow").
+      asJson();
+
+    System.out.println(stack.getBody().getObject().toString(2));
+  }
 }
